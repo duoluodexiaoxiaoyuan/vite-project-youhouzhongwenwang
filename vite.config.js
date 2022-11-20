e58785db80b6bb6ae0bbfce0cb428dc5b6852f5f
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-11-20 15:04:03 +0800

    feature:油猴脚本工程化

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import monkey, { cdn } from 'vite-plugin-monkey';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    monkey({
+      entry: 'src/main.jsx',
+      userscript: {
+        icon: 'https://ts1.cn.mm.bing.net/th?id=OIP-C.hpjQBHE4wfYFA1nm4KhTDwAAAA&w=173&h=185&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
+        namespace: 'npm/vite-plugin-monkey',
+        match: ['https://bbs.tampermonkey.net.cn/*'],
+      },
+      build: {
+        externalGlobals: {
+          react: cdn.jsdelivr('React', 'umd/react.production.min.js'),
+          'react-dom': cdn.jsdelivr(
+            'ReactDOM',
+            'umd/react-dom.production.min.js',
+          ),
+        },
+      },
+    }),
+  ],
+});
