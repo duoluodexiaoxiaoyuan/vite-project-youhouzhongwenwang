ccd8167817bf551be53880143a5ff70a5d4aaf1d
duoluodexiaoxiaoyuan <1203125214@qq.com>
2022-11-26 13:00:11 +0800

    淘宝后台拍卖自动取消7天无理由勾选，然后自动提交

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,16 @@ import { useEffect } from 'react';
 import { useState, Fragment } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
-    let timer = setInterval(() => {
-      if(document.getElementById("postmessage")) {
-        let postMessageTextArea = document.getElementById("postmessage")
-        // 失去焦点的时候就需要判断一下输入的字符长度，如果小于10自动补全
-        postMessageTextArea.onblur = function() {
-          if (postMessageTextArea.value.length < 10) {
-            let str = "!!!!!!!!!!"
-            // 用户输入的长度
-            let inputValue = postMessageTextArea.value || ''
-            console.log(str.substring(0,10 -inputValue.length));
-            // 补全10个字符
-            postMessageTextArea.value = inputValue + str.substring(0,10 -inputValue.length) 
-          }
-        }
-      }
+    document.getElementsByClassName('J_Tags J_Tagsservice_block')[0].checked=true
+    setTimeout(() => {
+      document.getElementsByClassName('submit-btn text-btn')[0].click()
     }, 2000);
   }, [])
 
   return (
     <Fragment>
-      {/* <div className="App">
-        你好世界，我是脚本
-      </div> */}
+      <div>淘宝自动取消勾选然后自动提交</div>
     </Fragment>
   );
 }
